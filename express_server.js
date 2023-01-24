858830db9ba1ef07ce4aceaa89e52a3870feb058
Connor <cgbroadfoot@gmail.com>
2023-01-24 16:22:00 +0000

    added urls_new.ejs to repo. Updated express_server.js to render urls_new.ejs and created the start of a post for new URL submission

--- express_server.js
+++ express_server.js
@@ -1,24 +1,50 @@
+
+
+function generateRandomString() {
+  
+  const randomString = Math.random().toString(36).substring(2,8);
+  return randomString
+} 
+
+// Express constants and config
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }));
 
+// App database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+/************************* Endpoint config *************************/
+
+// All URLs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Adding a URL
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(generateRandomString(req.body.longURL))
+  res.send('Oh ya'); // Respond with 'Ok' (we will replace this)
+});
+
+// A specific URL by ID
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+
+// Test Code
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
